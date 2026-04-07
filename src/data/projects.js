9b8ed29304ab67c56205eeca654df95446d7c2f4
Myklotuv <velasquez.michaelchristopher@gmail.com>
2026-04-07 15:53:38 +0800

    add project data

---
+++ src/data/projects.js
@@ -0,0 +1,33 @@
+import ccc from "../assets/projects/ccc.png";
+import edsabdg from "../assets/projects/edsabdg.png";
+import planaspantry from "../assets/projects/planaspantry.png";
+import inthezone from "../assets/projects/inthezone.png";
+
+const project = [
+    {
+        id: 1,
+        title: "CCC",
+        description: "Profile and Presentation website",
+        image: ccc,
+    },
+    {
+        id: 2,
+        title: "EDSABDG",
+        description: "Shopify-based business website.",
+        image: edsabdg,
+    },
+    {
+        id: 3, 
+        title: "Planas Pantry",
+        description: "Food business with improved layout and flow.",
+        image: planaspantry,
+    },
+    {
+        id: 4,
+        title: "InTheZoneToyCenter",
+        description: "Ongoing Shopify Store Project.",
+        image: inthezone,
+    },
+];
+
+export default projects;
