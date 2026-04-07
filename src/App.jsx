b1bf787f31704c74d2116dea921f0ddaefcca1e5
Myklotuv <velasquez.michaelchristopher@gmail.com>
2026-04-07 16:14:15 +0800

    add updated ProjectGridSection in app

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import ProjectGridSection from "./sections/ProjectGridSection";
 export default function App() {
   return (
     <main>
-      <ProjectGridSection />
+      <ProjectGridSection 
+        heading="Selected Work"
+        text="A few Shopify-related projects and store builds."
+        cardsToShow={4}
+        sectionAlign="left"
+        cardTextAlign="left"
+        imageRatio="4 / 3"
+      />
     </main>
   );
 }
